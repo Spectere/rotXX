9e811ed09747c55d4837a86a207024ba85ad680d
Ian Burgmyer <spectere@gmail.com>
2016-02-08 14:06:11 -0500

    Introduced some ternary abuse.

--- rotXX.c
+++ rotXX.c
@@ -6,8 +6,7 @@ main(int C, char** V) {
 	while((c = getchar()) != EOF) {
 		if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
 			c += r;
-			if(c > 122 && c - r >= 97) c -= 26;
-			else if(c > 90 && c - r <= 90) c -= 26;
+			c > 122 && c - r >= 97 ? c -= 26 : c > 90 && c - r <= 90 ? c -= 26 : c;
 		}
 
 		putchar(c);
