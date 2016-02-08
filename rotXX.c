54e6816e5b36db905e26d68ae994a792c6fa2dfb
Ian Burgmyer <spectere@gmail.com>
2016-02-07 22:26:17 -0500

    Shortened variable names.

--- rotXX.c
+++ rotXX.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(int argc, char** argv) {
-	int ch, rot = (argc > 1 ? atoi(argv[1]) : 13);
-	while((ch = getchar()) != EOF) {
-		if((ch >= 65 &&	ch <= 90) || (ch >= 97 && ch <= 122)) {
-			ch += rot;
-			if(ch > 122 && ch - rot >= 97) ch -= 26;
-			else if(ch > 90 && ch - rot <= 90) ch -= 26;
+void main(int C, char** V) {
+	int c, r = (C > 1 ? atoi(V[1]) : 13);
+	while((c = getchar()) != EOF) {
+		if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+			c += r;
+			if(c > 122 && c - r >= 97) c -= 26;
+			else if(c > 90 && c - r <= 90) c -= 26;
 		}
 
-		putchar(ch);
+		putchar(c);
 	}
 }
 
