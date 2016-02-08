849717ade6894ec25c18287eea0c790e5d82d077
Ian Burgmyer <spectere@gmail.com>
2016-02-08 14:45:40 -0500

    Removed whitespace.

--- rotXX.c
+++ rotXX.c
@@ -1,6 +1,1 @@
-main(int C, char** V) {
-	int c, r = (C > 1 ? atoi(V[1]) : 13);
-	for(;c>-1;c=getchar())
-		putchar((c >= 65 && c <= 90) || (c >= 97 && c <= 122) ? c -= c + r > 122 ? 26 - r : c + r > 90 && c <= 90 ? 26 - r : -r : c);
-}
-
+main(int C,char** V){int c,r=(C>1?atoi(V[1]):13);for(;c>-1;c=getchar())putchar((c>=65&&c<=90)||(c>=97&&c<=122)?c-=c+r>122?26-r:c+r>90&&c<=90?26-r:-r:c);}
