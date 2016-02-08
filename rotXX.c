26f47a7f3236c806cf2493684677b694eea570d1
Ian Burgmyer <spectere@gmail.com>
2016-02-08 14:12:26 -0500

    Saved another byte by replacing EOF check with <0

--- rotXX.c
+++ rotXX.c
@@ -3,7 +3,7 @@
 
 main(int C, char** V) {
 	int c, r = (C > 1 ? atoi(V[1]) : 13);
-	for(;c!=EOF;c=getchar()) {
+	for(;c<0;c=getchar()) {
 		if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
 			c += r;
 			c > 122 && c - r >= 97 ? c -= 26 : c > 90 && c - r <= 90 ? c -= 26 : c;
