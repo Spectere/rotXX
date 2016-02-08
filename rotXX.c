814e54327defe1e074c7722bdc95d6fe16d61faf
Ian Burgmyer <spectere@gmail.com>
2016-02-07 22:22:24 -0500

    Removed useless parameter check; set rot variable with a ternary; condensed variable declarations to one line.

--- rotXX.c
+++ rotXX.c
@@ -2,13 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-	int ch;
-	int rot = 13;
-	if(argc > 1) rot = atoi(argv[1]);
-	if(rot > 26 || rot < 0) {
-		printf("rot value cannot be greater than 26 or less than 0\n");
-		return 1;
-	}
+	int ch, rot = (argc > 1 ? atoi(argv[1]) : 13);
 	while((ch = getchar()) != EOF) {
 		if((ch >= 65 &&	ch <= 90) || (ch >= 97 && ch <= 122)) {
 			ch += rot;
