b3cdc17fbe9f9d520bbcf9ca49ccf5b0afada3e5
Ian Burgmyer <spectere@gmail.com>
2016-02-08 14:42:50 -0500

    Shoved everything into the putchar parameters.

--- rotXX.c
+++ rotXX.c
@@ -3,10 +3,7 @@
 
 main(int C, char** V) {
 	int c, r = (C > 1 ? atoi(V[1]) : 13);
-	for(;c>-1;c=getchar()) {
-		(c >= 65 && c <= 90) || (c >= 97 && c <= 122) ? c -= c + r > 122 ? 26 - r : c + r > 90 && c <= 90 ? 26 - r : -r : c;
-
-		putchar(c);
-	}
+	for(;c>-1;c=getchar())
+		putchar((c >= 65 && c <= 90) || (c >= 97 && c <= 122) ? c -= c + r > 122 ? 26 - r : c + r > 90 && c <= 90 ? 26 - r : -r : c);
 }
 
