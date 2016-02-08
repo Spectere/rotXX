3c0f0e3f7c736154d7161c7601214bf9015f6b8d
Ian Burgmyer <spectere@gmail.com>
2016-02-08 14:40:56 -0500

    Converted if to ternary.

--- rotXX.c
+++ rotXX.c
@@ -4,8 +4,7 @@
 main(int C, char** V) {
 	int c, r = (C > 1 ? atoi(V[1]) : 13);
 	for(;c>-1;c=getchar()) {
-		if((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-			c -= c + r > 122 ? 26 - r : c + r > 90 && c <= 90 ? 26 - r : -r;
+		(c >= 65 && c <= 90) || (c >= 97 && c <= 122) ? c -= c + r > 122 ? 26 - r : c + r > 90 && c <= 90 ? 26 - r : -r : c;
 
 		putchar(c);
 	}
